dc96d04006ff44163b76cd69211c3e8e44b41e40
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-04-23 14:09:53 -0400

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,11 @@
-(() => {
-    const refs = {
-      // Додати атрибут data-modal-open на кнопку відкриття
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      // Додати атрибут data-modal-close на кнопку закриття
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      // Додати атрибут data-modal на бекдроп модалки
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-      refs.modal.classList.toggle('is-open');
-    }
-  })();
+const openBtn = document.querySelector(".buttom-main-link");
+const closeBtn = document.querySelector("#closeModal");
+const modal = document.querySelector("#modal");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("is-open");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("is-open");
+});
