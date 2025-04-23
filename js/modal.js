d871c1eb6c617f9df60e494d77eeeba01f05b750
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-04-23 14:18:07 -0400

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,23 @@
-const openBtn = document.querySelector(".buttom-main-link");
-const closeBtn = document.querySelector("#closeModal");
-const modal = document.querySelector("#modal");
+document.addEventListener("DOMContentLoaded", () => {
+  const openBtn = document.querySelector(".buttom-main-link");
+  const closeBtn = document.querySelector("#closeModal");
+  const modal = document.querySelector("#modal");
+  const form = document.querySelector(".modal-form");
 
-openBtn.addEventListener("click", () => {
-  modal.classList.add("is-open");
-});
+  openBtn.addEventListener("click", () => {
+    modal.classList.add("is-open");
+  });
 
-closeBtn.addEventListener("click", () => {
-  modal.classList.remove("is-open");
+  closeBtn.addEventListener("click", () => {
+    modal.classList.remove("is-open");
+  });
+
+  if (form) {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      console.log("Форма відправлена!");
+      modal.classList.remove("is-open");
+      form.reset();
+    });
+  }
 });
